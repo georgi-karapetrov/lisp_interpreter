309b8f9084489da7ed347e043c16b621a712ec7a
Georgi Karapetrov <gkarapetrov@Georgis-MacBook-Pro-2.local>
2017-02-08 16:16:45 +0200

    Adds 'define' symbol

--- core/evaluator.rb
+++ core/evaluator.rb
@@ -8,8 +8,15 @@ class Evaluator
   def evaluate(exp)
     return exp if exp.is_a? Numeric
     return @env[exp] if exp.is_a? Symbol
+    
+    case exp[0]
+    when :define
+      _, var, e = exp
+      @env[var] = evaluate(e)
+    end
 
     code = evaluate(exp[0])
     args = exp[1..-1].map { |arg| eval(arg) }
+    code.(*args)
   end
 end
