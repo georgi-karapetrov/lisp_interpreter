1090195ddef224381fb62c0bd1bac627b5673ea8
Georgi Karapetrov <gkarapetrov@Georgis-MacBook-Pro-2.local>
2017-08-23 12:18:23 +0300

    Adds primitives

--- utils/env_primitives.rb
+++ utils/env_primitives.rb
@@ -1,8 +1,9 @@
 LIST_PRIMITIVES = {
-  list: ->(*list) { Array.new(list) },
+  list: ->(*list) { Array(list) },
   car: ->(*list) { list[0] },
   cdr: ->(*list) { list.drop(1) },
   cons: ->(head, *tail) { [head] + tail },
+  length: ->(*list) {list.flatten.length},
 }
 
 OPERATORS = {
@@ -12,4 +13,21 @@ OPERATORS = {
   :/ => ->(*list) { list.reduce(:/) },
   :== => ->(lhs, rhs) { lhs == rhs },
   :!= => ->(lhs, rhs) { lhs != rhs },
+  :> => ->(lhs, rhs) { lhs > rhs },
+  :< => ->(lhs, rhs) { lhs < rhs },
+  :>= => ->(lhs, rhs) { lhs >= rhs },
+  :<= => ->(lhs, rhs) { lhs <= rhs },
+  :quotient => ->(lhs, rhs) { (lhs / rhs).truncate },
+  :remainder => ->(lhs, rhs) { lhs != rhs },
+  :modulo => ->(lhs, rhs) { lhs % rhs },
+  :numerator => ->(lhs) { Rational(lhs).numerator },
+  :denominator => ->(lhs) { Rational(lhs).denominator },
+  :abs => ->(lhs) { lhs.abs }
+}
+
+BOOLEAN = {
+  :not => ->(exp) { not exp },
+  :equal? => ->(lhs, rhs) { lhs == rhs },
+  "#t" => true,
+  "#f" => false,
 }
